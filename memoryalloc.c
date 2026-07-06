55246e0643eda6cde3327a3af64b4b3873d75d05
Jamie Binskin <binskinjamie@gmail.com>
2026-07-06 16:35:50 +1000

    Added Memory Allocator Test

--- memoryalloc.c
+++ memoryalloc.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void memory_alloc(int size) {
+
+}
+
+//Test that it works
+int main(void) {
+    int number;
+    printf("Number of integers to store? ");
+    scanf("%d", &number);
+//Replace malloc with custom allocator
+    int* arr = malloc(number * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory Allocation Failed");
+    }
+//print the array
+    for (int i=0; i<number;i++) {
+        printf("Enter a number: ");
+        scanf("%d", &arr[i]);
+    }
+    for (int i=0; i<number;i++) {
+        printf("%d\n", arr[i]);
+    }
+
+//Replace with custom free()
+    free(arr);
+    arr = NULL;
+    return 0;
+}
